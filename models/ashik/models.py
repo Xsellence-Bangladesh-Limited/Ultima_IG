7798cc386013ba6576dd9ce874410a3592536c0a
Ashik <ashk11534@gmail.com>
2024-07-29 18:50:45 +0600

    Contact us page backend 90%.

--- models/ashik/models.py
+++ models/ashik/models.py
@@ -1,7 +1,25 @@
 from odoo import fields, models, api
 
-class Test(models.Model):
-    _name = 'ultima.ig.test'
-    _description = 'ultima.ig.test'
+class UsersMessage(models.Model):
+    _name = 'ultima.users.message'
+    _description = 'ultima.users.message'
+    _rec_name = 'email'
+    _order = 'id desc'
 
-    name = fields.Char()
+    first_name = fields.Char(string='First name')
+    last_name = fields.Char(string='Last name')
+
+    email = fields.Char(string='Email')
+    phone_number = fields.Char(string='Phone number')
+
+    message = fields.Text(string='Message')
+
+    date = fields.Datetime(default=lambda self: fields.Datetime.now(), string='Date')
+
+class ContactUsPageSettings(models.Model):
+    _name = 'ultima.contact.us.page.settings'
+    _description = 'ultima.contact.us.page.settings'
+
+    form_illustration = fields.Image(string='Form illustration')
+
+
