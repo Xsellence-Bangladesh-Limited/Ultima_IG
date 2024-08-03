49c0714de362512262b71a4bf9f974caa51b1f35
Ashik <ashk11534@gmail.com>
2024-08-04 00:06:17 +0600

    Service request page b + f 100%.

--- controllers/ashik/service_request.py
+++ controllers/ashik/service_request.py
@@ -3,12 +3,67 @@ from odoo.http import request as req
 
 
 class ServiceRequest(http.Controller):
-    @http.route('/service-request', type='http', auth='public')
-    def service_request(self):
+    @http.route('/service-request', type='http', auth='public', csrf=False)
+    def service_request(self, **form_data):
+
+        # Retrieving form points (start)
+        form_points = req.env['ultima.service.request.form.point'].sudo().search([], order='id asc')
+        # Retrieving form points (end)
+
+        # Retrieving features (start)
+        service_request_features = req.env['ultima.service.request.feature'].sudo().search([], order='id asc')
+        # Retrieving features (end)
+
         # Retrieving testimonial slides (start)
-        service_request_testimonial_slides = req.env['ultima.service.request.testimonial.slider'].sudo().search([], order='id asc')
+        service_request_testimonial_slides = req.env['ultima.service.request.testimonial.slider'].sudo().search([],
+                                                                                                                order='id asc')
         # Retrieving testimonial slides (end)
 
+        # Retrieving settings (start)
+        service_request_settings = req.env['ultima.service.request.settings'].sudo().search([], order='id desc', limit=1)
+        # Retrieving settings (end)
+
+        if req.session.get('service_form_submitted'):
+            req.session['service_form_submitted'] = False
+            return req.render('ultima.ultima_service_request_template', {
+                'form_points': form_points,
+                'service_request_features': service_request_features,
+                'service_request_testimonial_slides': service_request_testimonial_slides,
+                'service_request_settings': service_request_settings
+            })
+
+        if req.httprequest.method == 'POST':
+            full_name = form_data.get('requester_name').strip() if form_data.get('requester_name') else ''
+            mobile_number = form_data.get('requester_mobile_number').strip() if form_data.get(
+                'requester_mobile_number') else ''
+            preferred_date = form_data.get('requester_preferred_date').strip() if form_data.get(
+                'requester_preferred_date') else ''
+            preferred_time = form_data.get('requester_preferred_time').strip() if form_data.get(
+                'requester_preferred_time') else ''
+
+            req.session['service_form_submitted'] = True
+
+            # Creating a new record in the ultima.service.request table (start)
+            req.env['ultima.service.request'].sudo().create({
+                'full_name': full_name,
+                'registered_mobile_number': mobile_number,
+                'preferred_date': preferred_date,
+                'preferred_time': preferred_time
+            })
+            return req.render('ultima.ultima_service_request_template', {
+                'request_sent_successful': True,
+                'form_points': form_points,
+                'service_request_features': service_request_features,
+                'service_request_testimonial_slides': service_request_testimonial_slides,
+                'service_request_settings': service_request_settings
+            })
+            # Creating a new record in the ultima.service.request table (end)
+
+        req.session['service_form_submitted'] = False
+
         return req.render('ultima.ultima_service_request_template', {
-            'service_request_testimonial_slides': service_request_testimonial_slides
+            'form_points': form_points,
+            'service_request_features': service_request_features,
+            'service_request_testimonial_slides': service_request_testimonial_slides,
+            'service_request_settings': service_request_settings
         })
--- static/ashik/service_request/JS/script.js
+++ static/ashik/service_request/JS/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+
+   // Handling successful message sent alert(start)
+   $('.message-sent-successful-alert-cancel-btn').click(function(){
+       $('.message-sent-successful-alert').fadeOut('slow')
+   })
+
+   setTimeout(function(){
+       $('.message-sent-successful-alert').fadeOut('slow')
+   }, 3000)
+   // Handling successful message sent alert(end)
+
   // Testimonial Slider (start)
 
   var swiper = new Swiper(".mySwiper", {
