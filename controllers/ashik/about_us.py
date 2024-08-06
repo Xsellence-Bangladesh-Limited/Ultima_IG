cf17d51dd842d81546e59a340dc5aeac30a305d3
Ashik <ashk11534@gmail.com>
2024-08-06 20:04:02 +0600

    about us backend 100%, integration 95%.

--- controllers/ashik/about_us.py
+++ controllers/ashik/about_us.py
@@ -13,12 +13,22 @@ class AboutUs(http.Controller):
         about_us_features = req.env['ultima.about.us.feature'].sudo().search([], order='id asc')
         # Retrieving the features (end)
 
+        # Retrieving the growth data (start)
+        about_us_growth_data = req.env['ultima.about.us.growth.data'].sudo().search([], order='id asc')
+        # Retrieving the growth data (end)
+
         # Retrieving the offers (start)
         about_us_offers = req.env['ultima.about.us.offer'].sudo().search([], order='id asc')
         # Retrieving the offers (end)
 
+        # Retrieving the page settings (start)
+        about_us_settings = req.env['ultima.about.us.settings'].sudo([], order='id desc', limit=1)
+        # Retrieving the page settings (end)
+
         return req.render('ultima.ultima_about_us_template', {
             'about_us_clients': about_us_clients,
             'about_us_features': about_us_features,
-            'about_us_offers': about_us_offers
+            'about_us_growth_data': about_us_growth_data,
+            'about_us_offers': about_us_offers,
+            'about_us_settings': about_us_settings
         })
