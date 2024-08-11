fc9e0026c51e5c7312646b5e200cc7c91fb42c23
ashik <ashk11534@gmail.com>
2024-08-11 18:41:56 +0600

    Blog details backend 100%, blog details frontend 95%, blog_details frontend integration 100%.

--- controllers/ashik/blog.py
+++ controllers/ashik/blog.py
@@ -5,4 +5,29 @@ from odoo.http import request as req
 class Blog(http.Controller):
     @http.route('/blogs', type='http', auth='public')
     def blog(self):
-        return req.render('ultima.ultima_blog_template', {})
+
+        # Retrieving all the blogs (start)
+        all_blogs = req.env['ultima.blog.blog'].sudo().search([], order='id asc')
+        # Retrieving all the blogs (end)
+
+        # Retrieving blog slides (start)
+        blog_slides = req.env['ultima.blog.slider'].sudo().search([], order='id asc')
+        # Retrieving blog slides (end)
+
+        # Retrieving blog categories (start)
+        blog_categories = req.env['ultima.blog.category'].sudo().search([], order='id asc')
+        # Retrieving blog categories (end)
+
+        blogs_based_on_categories = {category.category_name: [] for category in blog_categories}
+
+        for blog in all_blogs:
+            blogs_based_on_categories[blog.category_id.category_name].append(blog)
+
+        print(blogs_based_on_categories)
+
+        return req.render('ultima.ultima_blog_template', {
+            'all_blogs': all_blogs,
+            'blog_slides': blog_slides,
+            'blog_categories': blog_categories,
+            'blogs_based_on_categories': blogs_based_on_categories
+        })
