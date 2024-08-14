3158d89d54bff50eeb81d8d624e51662e3cfa562
Abdur Razzak <razzak606@gmail.com>
2024-08-14 18:37:08 +0600

    home page bkend done

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'description': """Developed by Xsellence Bangladesh Limited.""",
     'category': '',
     'website': '',
-    'depends': [],
+    'depends': ['product'],
     'data': [
         'security/ir.model.access.csv',
         'security/razzak/ir.model.access.csv',
